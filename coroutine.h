68e0f036944d02270ebfe446f8251d4b7ad872af
miaoshua <945970809@qq.com>
2016-01-07 15:35:25 +0800

    线程的头文件

--- coroutine.h
+++ coroutine.h
@@ -11,6 +11,8 @@
 #include <ucontext.h>
 #include <map>
 #include <memory>
+#include <assert.h>
+#include <stdio.h>
 
 namespace mythread
 {
@@ -20,15 +22,30 @@ namespace mythread
             Mythread(int stackSize = 10240);
             ~Mythread();
             void start(void);
+
             template <typename threadCallback,typename ...Args>
-            void newThread(threadCallback cb,Args ...args);  //创建新线程
+            void newThread(threadCallback cb,Args ...args)  //创建新线程
+            {
+                printf("222\n");
+                std::unique_ptr<ucontext_t> threadContext(new ucontext_t);
+                threadContext->uc_stack.ss_sp = new char[stackSize_];
+                threadContext->uc_stack.ss_size = stackSize_;
+                threadContext->uc_link = 0;
+                threadContext->uc_flags = 0;
+                assert(threadContext->uc_stack.ss_sp != NULL);
+                makecontext(threadContext.get(),cb,sizeof...(args),args...);  //可能C++11多参与传统C不同
+                printf("222\n");
+                threadMap_.insert(std::pair<int,std::unique_ptr<ucontext_t>>(makeThreadId(),std::move(threadContext)));
+                printf("333\n"); 
+            }
+
             void destroyThread(int id);  //销毁某个线程
 
         private:
-            void threadSchedule(int n); //线程调度函数
+            static void threadSchedule(int n); //线程调度函数
             int makeThreadId(void);     //生成一个线程id
-            std::map<int,std::unique_ptr<ucontext>> threadMap_;  //保存线程id及其对应的线程上下文内容
-            int current_;                       //保存当前线程id
+            static std::map<int,std::unique_ptr<ucontext_t>> threadMap_;  //保存线程id及其对应的线程上下文内容
+            static int current_;                       //保存当前线程id
             int stackSize_;             //每个线程的栈空间的大小
             int maxThreadNumber_;               //最大可创建线程数
     };
