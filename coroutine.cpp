54bbac893329fd5f077d1314565ef1e8b0e30d72
miaoshua <945970809@qq.com>
2016-01-07 16:09:19 +0800

    线程类的实现

--- coroutine.cpp
+++ coroutine.cpp
@@ -12,12 +12,17 @@
 #include <sys/time.h>
 #include <signal.h>
 #include <functional>
+#include <memory>
+#include <stdio.h>
 
 using namespace mythread;
 
+std::map<int,std::unique_ptr<ucontext_t>> Mythread::threadMap_;
+int Mythread::current_ = 1;
+
+
 Mythread::Mythread(int stackSize)
-    :stackSize_(stackSize),
-    current_(0)
+    :stackSize_(stackSize)
 {
     
 }
@@ -36,29 +41,17 @@ void Mythread::start(void)
     std::unique_ptr<ucontext_t> mainContextPtr(new ucontext_t);
     threadMap_.insert(std::pair<int,std::unique_ptr<ucontext_t>>(1,std::move(mainContextPtr)));//将主线程加入到map中
 
-    signal(SIGALRM,std::bind(&Mythread::threadSchedule,this,std::placeholders::_1));
+    signal(SIGALRM,threadSchedule);
     
     struct itimerval tick;
     tick.it_value.tv_sec = 0;   //无定时器延时
-    tick.it_value.tv_usec = 0;
+    tick.it_value.tv_usec = 1;
     
     tick.it_interval.tv_sec = 0;
-    tick.it_interval.tv_usec = 10000;   //每10毫秒切一次线程    
+    tick.it_interval.tv_usec = 1000;   //每10毫秒切一次线程 
+    setitimer(ITIMER_REAL,&tick,NULL);
 }
 
-void Mythread::newThread(threadCallback cb,Args ...args)
-{
-    std::unique_ptr<ucontext_t> threadContext(new ucontext_t);
-    threadContext->uc_stack.ss_sp = new char[stackSize_];
-    threadContext->uc_stack.ss_size = stackSize_;
-    threadContext->uc_link = 0;
-    threadContext->uc_flags = 0;
-    assert(threadContext->uc_stack.ss_sp != NULL);
-    
-    makecontext(threadContext,cb,args...);  //可能C++11多参与传统C不同
-    
-    threadMap_.insert(std::pair<int,std::unique_ptr<ucontext_t>>(makeThreadId(),std::move(threadContext)));
-}
 
 void Mythread::destroyThread(int id)
 {
@@ -70,10 +63,12 @@ int Mythread::makeThreadId(void)
 {
     if((--threadMap_.end())->first != threadMap_.size())    //说明map中有未利用的id
     {
-        for(int i = 1,auto it = threadMap_.begin(); it != threadMap_.end(); i++,it++)
+        int i = 1;
+        for(auto it = threadMap_.begin(); it != threadMap_.end(); i++,it++)
         {
             if(i != it->first)
             {
+                printf("return  = %d\n",i);
                 return i;   
             }
         }
@@ -88,16 +83,19 @@ int Mythread::makeThreadId(void)
 
 void Mythread::threadSchedule(int n)
 {
-    int lastId = current_;  //保存当前id
-    if(current_ != (--threadMap_.end())->first)//当前线程不是map中的最后一个
+    if(threadMap_.size() > 1)
     {
-        auto it = threadMap_.find(current_);
-        current_ = (++it)->first;
-    }
-    else
-    {
-        current_ = threadMap_.begin()->first;   //从map的起点开始
-    }
+        int lastId = current_;  //保存当前id
+        if(current_ != (--threadMap_.end())->first)//当前线程不是map中的最后一个
+        {
+            auto it = threadMap_.find(current_);
+            current_ = (++it)->first;
+        }
+        else
+        {
+            current_ = threadMap_.begin()->first;   //从map的起点开始
+        }
 
-    swapcontext(threadMap_[lastId].get(),threadMap_[current_].get());   //切换线程
+        swapcontext(threadMap_[lastId].get(),threadMap_[current_].get());   //切换线程
+    }
 }
